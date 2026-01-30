ec9193e234a636550ebcfcd09cfdbd055ad3cca9
samsungsgp2019 <samsungsgp2019@gmail.com>
2026-01-30 22:14:56 +0000

    PrimerApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,18 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/rendering.dart';
 
-void main() => runApp(MiappRegistro());
-
-class MiappRegistro extends StatelessWidget {
-  const MiappRegistro({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Registro González",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      //rutas nombradas
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const PantallaBienvenida(),
-        '/datos': (context) => const PantallaDatos(),
-        '/final': (context) => const PantallaFinal(),
-      }, //Fin de rutas nombradas
-    );
-  } //Fin widget build
-} // Fin clase MiappRegistro
-
-class PantallaBienvenida extends StatelessWidget {
-  const PantallaBienvenida({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Bienvenido González 114"),
-        backgroundColor: Colors.amber,
-      ),
-      body:Center(child: ElevatedButton(
-        onPressed: ()=>Navigator.pushNamed(context, '/datos'),
-        child:Text('Empezar Registro'),
-
-      ),
-      ),
-
-    );
-  } 
-} // Fin pantalla Bienvenido
-class PantallaDatos extends StatelessWidget {
-  const PantallaDatos({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Datos personales"),
-        backgroundColor: Colors.blue,
-      ),
-      body:Center(
-        child: ElevatedButton(
-        onPressed: ()=>Navigator.pushNamed(context, '/final'),
-        child:Text('Finalizar Registro'),
-      ),
-      ),
-    );
-  }
-}//Fin Pantalla Datos
-class PantallaFinal extends StatelessWidget {
-  const PantallaFinal({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-        title: const Text("Final"),
-        backgroundColor: Colors.green,
-      ),
-    body:Center(
-        child: ElevatedButton(
-        onPressed: ()=>Navigator.pushNamed(context, '/'),
-        child:Text('Volver a Bienvenida'),
-    ),
-    ),
-    );
-  }
-}
+void main() {
+  runApp(
+    
+    MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Sergio CBTis 114 '),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Text( "Bienvenido a mi App Sergio",
+            )    
+          ),
+      )));
+} //fin de main  Shift + Alt + F Ordena líneas de código
