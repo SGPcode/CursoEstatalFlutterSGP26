967e8cdb7f28d59f0528af4ad66db1177fea909f
samsungsgp2019 <samsungsgp2019@gmail.com>
2026-02-02 18:13:37 +0000

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,101 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MiTienda());
-class MiTienda extends StatelessWidget {
-  const MiTienda({super.key});
+void main() => runApp(AppViajes());
+
+class AppViajes extends StatelessWidget {
+  const AppViajes({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
+      // TEMA Y COLOR
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        scaffoldBackgroundColor: Colors.grey[200],
+      ),
+      home: PantallaInicio(),
+    );
+  }
+}// fin clase AppViajes
 
-          backgroundColor:Colors.blue ,
-          leading: Icon(Icons.add_a_photo),
-          leadingWidth: 100,
-          title: Text('Mi tienda Sergio # 114'), 
-          centerTitle: true
-          ),
-        
-        body: Center(child: Text('Lista de Productos Disponibles')),
+class PantallaInicio extends StatelessWidget {
+  const PantallaInicio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Destinos Turísticos Sergio González Pérez Cbtis 114"),
+        backgroundColor: Colors.blue,
+        ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        // COLUMNA PRINCIPAL
+        child: Column(
+          children: [
+            // FILA 1
+            Row(
+              children: [
+                Expanded(child: TarjetaLugar(
+                  titulo: "Tenochtitlan, México,Sergio González Pérez Cbtis 114",
+                  desc: "Lugar donde abundan los nopales sobre piedra",
+                  url: "https://i.postimg.cc/SRcrkV8W/Tenochtitlan.jpg",
+                )),
+              ],
+            ),
+            // FILA 2
+            Row(
+              children: [
+                Expanded(child: TarjetaLugar(
+                  titulo: "Chichén Itzá, México Sergio González Pérez Cbtis 114",
+                  desc: "La ciudad al borde del pozo de los Itzáes",
+                  url: "https://i.postimg.cc/Y9q8NQJX/Chichen-Itza.jpg",
+                )),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(child: TarjetaLugar(
+                  titulo: "Paquimé, Chihuahua, México Sergio González Pérez Cbtis 114",
+                  desc: "El lugar de las casas grandes",
+                  url: "https://i.postimg.cc/zX0hW9J3/Paquime.jpg",
+                )),
+              ],
+            ),
+
+          ],
+        ),
       ),
     );
   }
-}
+}// fin de la clase PantallaInicio
+
+class TarjetaLugar extends StatelessWidget {
+  final String titulo, desc, url;
+  const TarjetaLugar({super.key, required this.titulo, required this.desc, required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row( // Row dentro de la card para poner imagen a la izquierda
+          children: [
+            Image.network(url, width: 80, height: 80, fit: BoxFit.cover),
+            SizedBox(width: 15),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(titulo, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                Text(desc),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}// fin de la clase TarjetaLugar
